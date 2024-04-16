25f718297ec1b48bac6ec19716bd16d8b1455dc0
bknudson1 <69976544+bknudson1@users.noreply.github.com>
2024-04-16 16:54:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBA9NaJh_qTWYwsVbnI5mSACZVRcSpWsio",
+  apiKey: "...",
   authDomain: "datadrobe.firebaseapp.com",
   projectId: "datadrobe",
   storageBucket: "datadrobe.appspot.com",
